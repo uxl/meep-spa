7fdb86ded57bb8dc5c098dc875ef555d337e69ff
uxl <brian@ulx.io>
2016-02-26 15:47:17 -0500

    remove click

--- app/scripts/tclicks.js
+++
@@ -1,8 +0,0 @@
-//touch click helper
-(function ($) {
-    $.fn.tclick = function (onclick) {
-        this.bind("touchstart", function (e) { onclick.call(this, e); e.stopPropagation(); e.preventDefault(); });
-        this.bind("click", function (e) { onclick.call(this, e); });   //substitute mousedown event for exact same result as touchstart
-        return this;
-    };
-})(jQuery);
